1978f5b2f85f6045569d688c7792c47b85cd4dcc
Kamocca <86765533+Kamocca1@users.noreply.github.com>
2025-04-24 22:55:12 +0200

    feat: Add function to sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (int1, int2) {
+    totalSum = 0;
+    if (int1 < 0 || int2 < 0) {
+        return "ERROR";
+    } else if (!Number.isInteger(int1) || !Number.isInteger(int2)) {
+        return "ERROR";
+    } else if (typeof int1 !== "number" || typeof int2 !== "number") {
+        return "ERROR";
+    } else if (int2 > int1) {
+        for (let i = int1; i < int2 + 1; i++) {
+            totalSum += i;
+        }
+        return totalSum;
+    } else {
+        for (let i = int2; i < int1 + 1; i++) {
+            totalSum += i;
+        }
+        return totalSum;
+    }
 };
 
 // Do not edit below this line
