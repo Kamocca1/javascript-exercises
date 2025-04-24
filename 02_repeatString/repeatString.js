9b796e79dc5a9a7c923395f835b9be991bb80f9f
Kamocca <86765533+Kamocca1@users.noreply.github.com>
2025-04-24 20:51:17 +0200

    feat: Create a function that repeats a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, repetitions) {
+    if (repetitions >= 0) {
+        finalString = "";
+        for (let i = 0; i < repetitions; i++) {
+            finalString += string;
+        }
+        return finalString;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
