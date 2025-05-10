275b4c3d8a790e874085a09a3e86164b234a225e
Kamocca <86765533+Kamocca1@users.noreply.github.com>
2025-05-10 14:39:36 +0200

    feat: Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,43 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+    const sum = array.reduce((total, number) => {
+        return total + number;
+    }, 0);
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+    const multiple = array.reduce((total, n) => {
+        return total * n;
+    }, 1);
+    return multiple;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+    if (n === 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
