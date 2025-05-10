4b41fce38bff156a4c6f78588dfbdc1389a6c09f
Kamocca <86765533+Kamocca1@users.noreply.github.com>
2025-05-10 15:13:17 +0200

    feat: Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+    const n = Number(number);
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    if (n === 1 || n === 2) return 1;
 
+    let a = 1,
+        b = 1;
+    for (let i = 3; i <= n; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
 };
 
 // Do not edit below this line
