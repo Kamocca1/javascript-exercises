333943f84d85022d886ad27af0fed41b4417be4a
Kamocca <86765533+Kamocca1@users.noreply.github.com>
2025-05-10 14:57:35 +0200

    feat: Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let copyWithSpacesAndCapitalization = string.replace(/[^\w\s]|_/g, "");
+    let copyWithSpaces = copyWithSpacesAndCapitalization.toLowerCase();
+    let copy = copyWithSpaces.split(" ").join("");
+    let reversed = copy.split("").reverse().join("");
+    return copy === reversed;
 };
 
 // Do not edit below this line
