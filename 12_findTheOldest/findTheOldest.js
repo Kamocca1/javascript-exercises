27828a01a7a8143beff554f701baf52afd711547
Kamocca <86765533+Kamocca1@users.noreply.github.com>
2025-05-10 15:42:50 +0200

    feat: Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    const ageDescending = people.sort((a, b) => {
+        let aYears;
+        let bYears;
 
+        if (!a.yearOfDeath) {
+            aYears = new Date().getFullYear() - a.yearOfBirth;
+        } else {
+            aYears = a.yearOfDeath - a.yearOfBirth;
+        }
+
+        if (!b.yearOfDeath) {
+            bYears = new Date().getFullYear() - b.yearOfBirth;
+        } else {
+            bYears = b.yearOfDeath - b.yearOfBirth;
+        }
+
+        return aYears > bYears ? -1 : 1;
+    });
+    const oldest = ageDescending[0];
+    return oldest;
 };
 
 // Do not edit below this line
